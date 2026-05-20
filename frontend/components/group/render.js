b57b5e44575648d678555ad8a288ef3e2de063e8
Hamza Zahid <hamzazahid0066@gmail.com>
2026-05-20 23:30:53 +0500

    Set inner canvas pointer-events to none to fix move operation

--- frontend/components/group/render.js
+++ frontend/components/group/render.js
@@ -15,6 +15,7 @@ window.GroupComponent.render = function(component) {
   el.style.boxSizing = "border-box";
   el.style.width = "100%";
   el.style.height = "100%";
+  el.style.position = "relative";
 
   // Create inner canvas for children
   const innerCanvas = document.createElement("div");
